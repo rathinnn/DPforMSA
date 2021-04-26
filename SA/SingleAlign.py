8f0e54832fc462a88d66b2283053084d96342516
rathinnn <rathinnnaiir77@gmail.com>
2021-04-26 21:57:42 +0530

    Added Traceback for Alignment

--- SA/SingleAlign.py
+++ SA/SingleAlign.py
@@ -12,6 +12,7 @@ def SAlign(Str1,Str2,Penalty,d):
     print(dp[m][n])
     print(tb)
     print(dp)
+    traceback(tb,dp,Str1,Str2,m,n)
 
 def iterate(dp,d,m,n,Penalty,Str1,Str2,tb):
     
@@ -22,39 +23,75 @@ def iterate(dp,d,m,n,Penalty,Str1,Str2,tb):
         for j in range(1,n+1):
             costindex1 = Index[Str1[i - 1]]
             costindex2 = Index[Str2[j - 1]]
-            (dp[i][j],tb[i][j]) = custmin( dp[i - 1][j] + d, dp[i][j - 1] + d, dp[i - 1][j - 1] + Penalty[costindex1][costindex2] , Str1[i-1]==Str2[j-1])
+            (dp[i][j],tb[i][j]) = custommin( dp[i - 1][j] + d, dp[i][j - 1] + d, dp[i - 1][j - 1] + Penalty[costindex1][costindex2])
 
 
-def custmin(a,b,c,match):
+def custommin(a,b,c):
     if(a<b):
         if(a<c):
-            return (a,0)
+            return (a,-1)
         else:
 
-            if(match):
-                return (c,1)
-            else:
-                return (c,-1)
+            return (c,1)
     else:
         if(b<c):
             return (b,0)
-        
         else:
-            if(match):
-                return (c,1)
-            else:
-                return (c,-1)
+            return (c,1)
+
+
 
 
 def initiallize(dp,d,m,n,tb):
     for i in range(1,m + 1):
         
         dp[i][0] = dp[i - 1][0] + d
-        tb[i][0] = 0
+        tb[i][0] = -1
         #print(dp[i][0])
         #print(dp)
 
     for i in range(1,n + 1):
         dp[0][i] = dp[0][i - 1] + d
         tb[0][i] = 0
+
+def traceback(tb,dp,Str1,Str2,m,n):
+    retStr1 = ""
+    retStr2 = ""
+    i = m
+    j = n
+    mini = 0
+    while(i!=0 or j!=0):
+        
+        minop = tb[i][j]
+        (retStr1, retStr2,i,j) = selectOp(Str1,retStr1,Str2,retStr2,minop,i,j)
+        
+    print(retStr1)
+    print(retStr2)
+
+
+def selectOp(Str1,retStr1,Str2,retStr2,minop,i,j):
+    if(minop == -1):
+        #print('Here')
+        retStr1 = Str1[i-1] + retStr1
+        retStr2 = '_' + retStr2
+        i=i-1
+    elif(minop == 0):
+        #print('Here2')
+        retStr2 = Str2[j-1] + retStr2
+        retStr1 = '_' + retStr1
+        j=j-1
+    else:
+        #print('Here3')
+        retStr1 = Str1[i-1] + retStr1
+        retStr2 = Str2[j-1] + retStr2
+        i=i-1
+        j=j-1
+    
+    return(retStr1,retStr2,i,j)
+
+
+
+    
+
+
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from SA.SingleAlign import SAlign
 
-Penalty = [[0,4,2,4],[4,0,4,2],[2,4,0,4],[4,2,4,0]]
-d = 8
+Penalty = [[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]]
+d = 2
 
-S1 = "TACGTCAGC"
-S2 = "TATGTCATGC"
+S1 = "AGT"
+S2 = "AAGC"
 
 SAlign(S1,S2,Penalty,d)
