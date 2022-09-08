c21d716efdb07ac7c54dee6ad68b54af6fdf3b1f
Ganesh R Nair <104490237+GaneshNair05@users.noreply.github.com>
2022-09-08 09:00:03 +0530

    Update MultipleAlign.py
    Corrected spacing between the lines

--- MSA/MultipleAlign.py
+++ MSA/MultipleAlign.py
@@ -19,8 +19,7 @@ def MAlign(AStrings,Penalty,d,usePenal):
     Strmemo = np.empty(dimensions, dtype = list)
     
     Align(Penalty,memo,tuple(indexes),AStrings,RetStrings,d,Strmemo,usePenal)
-    
-    
+
     print(Strmemo[tuple(nindexes)])
     print(memo[tuple(nindexes)])
     
@@ -28,10 +27,8 @@ def MAlign(AStrings,Penalty,d,usePenal):
     f.write(str(Strmemo[tuple(nindexes)]))
     f.close()
 
-        
 def Align(Penalty,memo,indexes,Strings,RetStrings,d,Strmemo,usePenal):
     global iters
-    
     iters = iters + 1
     
     (check,theString) = checkBaseCase(indexes)
@@ -46,11 +43,9 @@ def Align(Penalty,memo,indexes,Strings,RetStrings,d,Strmemo,usePenal):
         memo[indexes] = check*d*(len(indexes)-1)
         Strmemo[indexes] = ['_']*len(indexes)
         Strmemo[indexes][theString] = Strings[theString][0:check]
-        
-        
-        return check*d*(len(indexes)-1)
 
-    
+        return check*d*(len(indexes)-1)
+ 
     minval = 9999
     minchar = None
     minStrsaved = None
@@ -59,12 +54,9 @@ def Align(Penalty,memo,indexes,Strings,RetStrings,d,Strmemo,usePenal):
     newindex = []
     for i in range(len(indexes),0,-1):
         indices = list(combinations(range(len(indexes)),i))
-
-        
+ 
         for ind in indices:
-            
-            
-            
+ 
             previndex = newindex
             newindex = []
             for j in range(len(indexes)):
@@ -74,43 +66,28 @@ def Align(Penalty,memo,indexes,Strings,RetStrings,d,Strmemo,usePenal):
                 else:
                     newindex.append(indexes[j])
             newindex = tuple(newindex)
-            
-            
-            
-            
-            
+   
             if(memo[newindex] !=- 1 and newindex!=indexes and previndex!=newindex):
                 newval = memo[newindex] + getPenalty(Penalty,indexes,ind,Strings,usePenal)
-                
-                
+             
                 if(newval < minval):
-                    
-                    
-                    
+          
                     minval = newval
                     minchar = ind
                     
                     minStrsaved = Strmemo[newindex]
-                
-                    
-                    
+       
             elif(newindex!=indexes and previndex!=newindex):
                 
                 newval = Align(Penalty,memo,newindex,Strings,RetStrings, d,Strmemo,usePenal) + getPenalty(Penalty,indexes,ind,Strings,usePenal)
                 
                 if(newval < minval):
-                    
-                    
-                    
+                
                     minval = newval
                     minchar = ind
                     
                     minStrsaved = Strmemo[newindex]
-                
-                    
-                    
-        
-    
+     
     minStr = minStrsaved
     memo[indexes] = minval
     
@@ -124,12 +101,6 @@ def Align(Penalty,memo,indexes,Strings,RetStrings,d,Strmemo,usePenal):
             EvalChars.append('_')
     i=0
     
-    
-    
-    
-    
-    
-    
     for stri in minStr:
         stri = stri + EvalChars[i]
         
@@ -138,14 +109,7 @@ def Align(Penalty,memo,indexes,Strings,RetStrings,d,Strmemo,usePenal):
     Strmemo[indexes] = newStr
         
     return minval
-        
-        
-
-
-
-
-
-
+      
 def checkBaseCase(indexes):
     
     count = 0
@@ -165,8 +129,7 @@ def checkBaseCase(indexes):
 
 def getPenalty(Penalty,indexes,indices,Strings,usePenal):
     ACGTi = {'_':0, 'A':1, 'C':2, 'G':3, 'T':4}
-    
-    
+   
     EvalChars = []
     for j in range(len(indexes)):
         indic = j
@@ -185,7 +148,6 @@ def getPenalty(Penalty,indexes,indices,Strings,usePenal):
             index2 = ACGTi[evaluation[1]]
             totalPenalty += Penalty[index1][index2]
 
-    
     else:
         
         for evaluation in evaluations:
@@ -218,7 +180,3 @@ def verify(Strings,Penalty,usePenal):
                     totalPenalty+=1
     
     return totalPenalty
-    
-
-
-    
